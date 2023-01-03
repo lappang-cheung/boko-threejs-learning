a70118f7aeefc14a3787c4a7d596b574ad375d31
Leo Cheung <lappangcheung@gmail.com>
2023-01-03 16:24:02 -0500

    Adding resources to the code

--- Experience/Utils/Resources.js
+++ Experience/Utils/Resources.js
@@ -1,4 +1,7 @@
 import { EventEmitter } from 'events'
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
 import Experience  from '../Experience'
 
 export default class Resources extends EventEmitter{
@@ -15,5 +18,53 @@ export default class Resources extends EventEmitter{
 
     this.setLoaders()
     this.startLoading()
+  }
+
+  setLoaders() {
+    this.loaders = {}
+    this.loaders.gltfLoader = new GLTFLoader()
+    this.loaders.dracoLoader = new DRACOLoader()
+    this.loaders.dracoLoader.setDecoderPath('/draco/')
+    this.loaders.gltfLoader.setDRACOLoader(this.loaders.dracoLoader)
+  }
+  startLoading () {
+    for(const asset of this.assets) {
+      if(asset.type === 'glbModel'){
+        this.loaders.gltfLoader.load(asset.path, (file) => {
+          this.singleAssetLoader(asset, file)
+        })
+      } else if (asset.type === 'videoTexture') {
+        this.video = {}
+        this.videoTexture = {}
+
+        this.video[asset.name] = document.createElement('video')
+        this.video[asset.name].src = asset.path
+        this.video[asset.name].muted = true
+        this.video[asset.name].playsInline = true
+        this.video[asset.name].autoplay = true
+        this.video[asset.name].loop = true
+        this.video[asset.name].play()
+
+        this.videoTexture[asset.name] = new THREE.VideoTexture(
+          this.video[asset.name]
+        )
+
+        this.videoTexture[asset.name].flipY = true
+        this.videoTexture[asset.name].minFilter = THREE.NearestFilter
+        this.videoTexture[asset.name].magFilter = THREE.NearestFilter
+        this.videoTexture[asset.name].generateMipmaps = false
+        this.videoTexture[asset.name].encoding = THREE.sRGBEncoding
+      }
+    }
+  }
+  singleAssetLoader(asset, file) {
+    this.items[asset.name] = file
+    this.loaded++
+
+    console.log(file)
+
+    if (this.loaded === this.queue) {
+      this.emit('ready')
+    }
   }
 }
